9564d48d6eb1970d0175fb3628227a78dee76693
RenzoWille <renzo.wille@hva.nl>
2025-09-03 15:31:52 +0200

    #2

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+let { data } = $props();
+
+const members = data.members
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#each members as member}
+<a href="/{member.id}">
+    {member.name} - <br><img src="{member.avatar}" alt="student avatar">
+</a><br>
+{/each}
+
+<style>
+    img {
+        height: 200px;
+        width: 200px;
+    }
+</style>
+
+
+
---
+++ my-app/src/routes/[id]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { data } = $props()
+
+    const member = data.member
+</script>
+
+<a href="/">Back to home</a>
+
+
+<h1>{member.name}</h1>
