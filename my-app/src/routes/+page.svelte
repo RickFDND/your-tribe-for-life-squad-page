9bd519103b2079a87ca40d7313bc74c3549b1766
RenzoWille <renzo.wille@hva.nl>
2025-09-04 14:05:58 +0200

    #8 begin met wat styling

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,26 +1,58 @@
 <script>
+    let { data } = $props();
 
-let { data } = $props();
-
-const members = data.members
-
+    const members = data.members;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Squadpage</h1>
 
-{#each members as member}
-<a href="/{member.id}">
-    {member.name} - <br><img src="{member.avatar}" alt="student avatar">
-</a><br>
-{/each}
+<div class="members">
+    {#each members as member}
+        <a href="/{member.id}">
+            {member.name} - <br /><img
+                src={member.avatar}
+                alt="student avatar"
+            />
+        </a><br />
+    {/each}
+</div>
 
 <style>
-    img {
-        height: 200px;
-        width: 200px;
+    :global(body) {
+        background-color: #a675f5;
+    }
+
+    h1 {
+        text-transform: uppercase;
+        text-align: center;
+        color: #050542;
+        padding-top: 2rem;
+    }
+
+    .members {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        margin-top: 2em;
+    }
+
+    a {
+        display: flex;
+        flex-direction: column;
+        align-items: center; 
+        border: 2px solid #66e5bf;
+        height: 300px;
+        width: 250px;
+        text-align: center;
+        margin-bottom: 2rem;
+        text-decoration: none;
+        color: #66e5bf;
+
+
+        img {
+            height: 200px;
+            width: 200px;
+        }
     }
 </style>
-
-
-
